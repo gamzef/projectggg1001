52bbf47379b52d44d64d37e19e1dbd49bb5ee65e
gamzef <48602269+gamzef@users.noreply.github.com>
2020-01-18 23:59:46 +0300

    meine medikamente seite eklendi

---
+++ app/src/main/java/com/example/projectggg1001/MMedikamenteRecyclerAdapter.java
@@ -0,0 +1,78 @@
+package com.example.projectggg1001;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MMedikamenteRecyclerAdapter extends RecyclerView.Adapter<MMedikamenteRecyclerAdapter.MedikamenteHolder> {
+
+    private ArrayList<String> mnameList;
+    private ArrayList<String> mzeitList;    //ilaç saati
+    private ArrayList<Object> mwieoftList; //kaç saatte bir
+    private ArrayList<Object> mdosisList;
+    private ArrayList<Object> mlagerList;
+    private ArrayList<String> menddatumList;
+
+    public MMedikamenteRecyclerAdapter(ArrayList<String> mnameList, ArrayList<String> mzeitList, ArrayList<Object> mwieoftList, ArrayList<Object> mdosisList, ArrayList<Object> mlagerList, ArrayList<String> menddatumList) {
+        this.mnameList = mnameList;
+        this.mzeitList = mzeitList;
+        this.mwieoftList = mwieoftList;
+        this.mdosisList = mdosisList;
+        this.mlagerList = mlagerList;
+        this.menddatumList = menddatumList;
+    }
+
+    @NonNull
+    @Override
+    public MedikamenteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {      //ViwHolder oluşturulunca ne yapacağım?
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.recycler_row,parent,false);
+
+        return new MedikamenteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MedikamenteHolder holder, int position) {     //ViewHolder'a bağlanınca ne yapacağım?
+        holder.ilacismiText.setText(mnameList.get(position));
+        holder.ilacsaatiText.setText(mzeitList.get(position));
+        holder.ilaczamaniText.setText(""+mwieoftList.get(position));
+        holder.ilacdozuText.setText(""+mdosisList.get(position));
+        holder.ilackapsulsayiText.setText(""+mlagerList.get(position));
+        holder.ilactarihText.setText(menddatumList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {     //kaç tane row var?
+        return mnameList.size();
+    }
+
+    class MedikamenteHolder extends RecyclerView.ViewHolder{        //recyler_row'daki textview'ları burda tanımlıyoruz.
+
+        TextView ilacismiText;
+        TextView ilacsaatiText;
+        TextView ilaczamaniText;
+        TextView ilacdozuText;
+        TextView ilackapsulsayiText;
+        TextView ilactarihText;
+
+        public MedikamenteHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ilacismiText = itemView.findViewById(R.id.recyclerView_row_ilaçismi_text);
+            ilacsaatiText = itemView.findViewById(R.id.recyclerView_row_ilaçsaati_text);
+            ilaczamaniText = itemView.findViewById(R.id.recyclerView_row_ilaçzamanı_text);
+            ilacdozuText = itemView.findViewById(R.id.recyclerView_row_ilaçdozu_text);
+            ilackapsulsayiText = itemView.findViewById(R.id.recyclerView_row_ilaçkapsülsayısı_text);
+            ilactarihText = itemView.findViewById(R.id.recyclerView_row_ilaçtarih_text);
+        }
+    }
+
+}
--- app/src/main/java/com/example/projectggg1001/ui/mmedikamente/MMedikamenteFragment.java
+++ app/src/main/java/com/example/projectggg1001/ui/mmedikamente/MMedikamenteFragment.java
@@ -1,18 +1,24 @@
 package com.example.projectggg1001.ui.mmedikamente;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.projectggg1001.MMedikamenteRecyclerAdapter;
 import com.example.projectggg1001.R;
 import com.firebase.client.Firebase;
 import com.google.firebase.firestore.CollectionReference;
@@ -24,18 +30,27 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 public class MMedikamenteFragment extends Fragment {
 
     private MMedikamenteViewModel MMedikamenteViewModel;
     private FirebaseFirestore firebaseFirestore;
+    private ArrayList<String> mnameFromFB;
+    private ArrayList<String> mzeitFromFB;
+    private ArrayList<Object> mwieoftFromFB;
+    private ArrayList<Object> mdosisFromFB;
+    private ArrayList<Object> mlagerFromFB;
+    private ArrayList<String> menddatumFromFB;
+    MMedikamenteRecyclerAdapter mMedikamenteRecyclerAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         MMedikamenteViewModel =
                 ViewModelProviders.of(this).get(MMedikamenteViewModel.class);
         View root = inflater.inflate(R.layout.fragment_mmedikamente, container, false);
+        FragmentActivity c = getActivity();
         firebaseFirestore = FirebaseFirestore.getInstance();
         final TextView textView = root.findViewById(R.id.text_mmedikamente);
         MMedikamenteViewModel.getText().observe(this, new Observer<String>() {
@@ -44,7 +59,23 @@ public class MMedikamenteFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        mnameFromFB = new ArrayList<>();
+        mzeitFromFB = new ArrayList<>();
+        mwieoftFromFB = new ArrayList<>();
+        mdosisFromFB = new ArrayList<>();
+        mlagerFromFB = new ArrayList<>();
+        menddatumFromFB = new ArrayList<>();
+
         getDataFromFirestore();
+
+        //RecyclerView
+        RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(c);
+        recyclerView.setLayoutManager(layoutManager);      //recycler_row'la tek tek ekranda göstereceğimizi belirtiyoruz
+        mMedikamenteRecyclerAdapter = new MMedikamenteRecyclerAdapter(mnameFromFB,mzeitFromFB,mwieoftFromFB,mdosisFromFB,mlagerFromFB,menddatumFromFB);
+        recyclerView.setAdapter(mMedikamenteRecyclerAdapter);
+
         return root;
     }
 
@@ -56,13 +87,24 @@ public class MMedikamenteFragment extends Fragment {
                 if(queryDocumentSnapshots!=null){
                     for(DocumentSnapshot snapshot : queryDocumentSnapshots.getDocuments()){
                         Map<String,Object> mmedikamente = snapshot.getData();
+
+                        //Casting
                         String medikamentename = (String) mmedikamente.get("medikamentename");
-                        /*String medikamentezeit = (String) mmedikamente.get("medikamentezeit"+":"+"medikamenteminute");
-                        int medikamentewieoft = (int) mmedikamente.get("medikamentewieoft");
-                        int medikamentedosis = (int) mmedikamente.get("medikamentedosis");
-                        int medikamentelager = (int) mmedikamente.get("medikamentelager");
-                        String medikamenteenddatum = (String) mmedikamente.get("medikamenteenddatum");*/
-                        System.out.println(medikamentename);
+                        String medikamentezeit = (String) mmedikamente.get("medikamentezeit"+":"+"medikamenteminute");
+                        Object medikamentewieoft = (Object) mmedikamente.get("medikamentewieoft");
+                        Object medikamentedosis = (Object) mmedikamente.get("medikamentedosis");
+                        Object medikamentelager = (Object) mmedikamente.get("medikamentelager");
+                        String medikamenteenddatum = (String) mmedikamente.get("medikamenteenddatum");
+
+                        mnameFromFB.add(medikamentename);
+                        mzeitFromFB.add(medikamentezeit);
+                        mwieoftFromFB.add(medikamentewieoft);
+                        mdosisFromFB.add(medikamentedosis);
+                        mlagerFromFB.add(medikamentelager);
+                        menddatumFromFB.add(medikamenteenddatum);
+
+                        mMedikamenteRecyclerAdapter.notifyDataSetChanged();
+
                     }
                 }
             }
