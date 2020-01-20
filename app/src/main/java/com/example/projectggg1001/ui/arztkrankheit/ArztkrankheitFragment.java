aa61fe19828f3416118b032e8ea7233a864e688d
gamzef <48602269+gamzef@users.noreply.github.com>
2020-01-21 00:27:01 +0300

    Tüm designlar tamam

--- app/src/main/java/com/example/projectggg1001/ui/arztkrankheit/ArztkrankheitFragment.java
+++ app/src/main/java/com/example/projectggg1001/ui/arztkrankheit/ArztkrankheitFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,22 +16,37 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.projectggg1001.R;
 
+import org.w3c.dom.Text;
+
 public class ArztkrankheitFragment extends Fragment {
 
     private ArztkrankheitViewModel arztkrankheitViewModel;
+    private EditText teilname;
+    private EditText beschwerdename;
+    private TextView namearzt;
+    private Button ergebnis;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         arztkrankheitViewModel =
                 ViewModelProviders.of(this).get(ArztkrankheitViewModel.class);
         View root = inflater.inflate(R.layout.fragment_arztkrankheit, container, false);
-        final TextView textView = root.findViewById(R.id.text_arztkrankheit);
-        arztkrankheitViewModel.getText().observe(this, new Observer<String>() {
+
+        teilname = root.findViewById(R.id.teilname);
+        beschwerdename = root.findViewById(R.id.beschwerdename);
+        namearzt = root.findViewById(R.id.namearzt);
+        ergebnis = root.findViewById(R.id.ergebnis);
+
+        //Kullanıcının hangi doktora gideceğini gösterecek.
+        ergebnis.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                String name_teil = teilname.getText().toString().toLowerCase();
+                String name_beschwerde = beschwerdename.getText().toString().toLowerCase();
+
             }
         });
+
         return root;
     }
 }
--- app/src/main/java/com/example/projectggg1001/ui/profil/ProfilFragment.java
+++ app/src/main/java/com/example/projectggg1001/ui/profil/ProfilFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,21 +15,52 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.projectggg1001.R;
+import com.fasterxml.jackson.databind.util.BeanUtil;
 
 public class ProfilFragment extends Fragment {
 
     private ProfilViewModel profilViewModel;
+    private EditText nameText;
+    private EditText emailText;
+    private EditText geschlechtText;
+    private EditText geburtsdatumText;
+    private EditText gewichtText;
+    private EditText grosseText;
+    private EditText krankheitText;
+    private TextView passwortText;
+    private Button abbrechen;
+    private Button speichern;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         profilViewModel =
                 ViewModelProviders.of(this).get(ProfilViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profil, container, false);
-        final TextView textView = root.findViewById(R.id.text_profil);
-        profilViewModel.getText().observe(this, new Observer<String>() {
+
+        //Tanımlama
+        nameText = root.findViewById(R.id.nameText_pa);
+        emailText = root.findViewById(R.id.emailText_pa);
+        geschlechtText = root.findViewById(R.id.geschlechtText_pa);
+        geburtsdatumText = root.findViewById(R.id.gdatumText_pa);
+        gewichtText = root.findViewById(R.id.gewichtText_pa);
+        grosseText = root.findViewById(R.id.grosseText_pa);
+        krankheitText = root.findViewById(R.id.kranheitText_pa);
+        passwortText = root.findViewById(R.id.tvPasswortLink);
+        speichern = root.findViewById(R.id.btpa_speichern);
+
+        //Şifre değiştirme
+        passwortText.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Değiştirilen bilgileri kaydetme
+        speichern.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
         return root;
